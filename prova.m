ef34f91f07b0fc7d58a9b162c2fea54a4cc51edc
Annarchia <93920575+Annarchia@users.noreply.github.com>
2023-01-16 13:28:27 +0100

    uploaded the matlab scripts
    prova: equation 3 modelling the impulse injection of the drug
    model_trt: we need to figure out a way to make this work

--- prova.m
+++ prova.m
@@ -3,12 +3,11 @@ clear
 % X(1) = A(t), X(2) = E(t), X(3) = C(t)
 d = 1.25 * 2.4e18; % for Cyt 62.5 mg/kg
 
-Tf = 1000;
+Tf = 100;
 tspan = [0;Tf];
-X0 = [1e10,1e6,d];
+X0 = d;
 op1 = odeset('MaxStep',1);
-
-[t,X] = ode45(@model,tspan,X0);
+[t,X] = ode45(@model,tspan,X0,op1);
 
 figure;
 plot(t,X,'+')
@@ -17,8 +16,6 @@ xlabel('Time (h)')
 ylabel('Number')
 
 function dX = model(t,X)
-r = 0.01;
-K = 4e6;
 mu_ac = 0.012; % for Cyt 62.5 mg/kg
 mu_ca = mu_ac * 10;
 d = 1.25 * 2.4e18; % for Cyt 62.5 mg/kg
@@ -32,8 +29,11 @@ b = 5e6;
 mu_ea = 4e-15;
 mu_e = 4e-5;
 tau = 24;
+nInject = round(100/24, 0);
 
-dX = [r*X(1)*(1-(X(1)/K)) - mu_a*X(1)*X(2) - ((mu_ac*X(1)*X(3))/(a+X(3)));
-    -mu_e*X(2) + (p*X(1)*X(2))/(c+X(1)) - mu_ea*X(1)*X(2)  - (mu_ec*X(2)*X(3))/(b+X(3));
-    d*M(round(t/tau,0)) - mu_c*X(3) - (mu_ca*X(3)*X(1))/(a+X(1))];
+if rem(round(t(1),0),tau) == 0
+    dX = d - mu_c*X(1); 
+else
+    dX = - mu_c*X(1);
+end
 end
